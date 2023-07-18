28e1a3fd3c12b782f22b6c99c5965b6e8d52272a
lxhan <lxhan@naver.com>
2023-07-18 12:43:16 +0800

    add vpc zone id to asg

--- ecs_ec2/auto_scaling.tf
+++ ecs_ec2/auto_scaling.tf
@@ -10,6 +10,7 @@ resource "aws_launch_configuration" "main" {
 
 resource "aws_autoscaling_group" "main" {
   name                      = "${var.project_name}-${var.environment}-asg"
+  vpc_zone_identifier       = aws_subnet.public.*.id
   availability_zones        = var.availability_zones
   launch_configuration      = aws_launch_configuration.main.name
   min_size                  = var.app_min_count

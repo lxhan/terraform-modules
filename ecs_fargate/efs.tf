245bc62bd3f35c27d80788bf54942b800d0c2e4f
Alex Han <lx.han@ya.ru>
2025-01-24 18:01:27 +0900

    Update EFS mount target subnet assignment for conditional VPC support
    - Modified the subnet assignment in `efs.tf` to use the public subnet based on the `create_vpc` variable, ensuring flexibility in network configuration.
    - This change allows for better integration with existing VPC setups while maintaining the option to create a new VPC.

--- ecs_fargate/efs.tf
+++ ecs_fargate/efs.tf
@@ -10,7 +10,7 @@ resource "aws_efs_file_system" "main" {
 resource "aws_efs_mount_target" "main" {
   count          = var.create_efs ? 1 : 0
   file_system_id = aws_efs_file_system.main[0].id
-  subnet_id      = aws_subnet.private[0].id
+  subnet_id      = var.create_vpc ? aws_subnet.public[0].id : var.public_subnets[0]
 }
 
 resource "aws_efs_backup_policy" "policy" {
